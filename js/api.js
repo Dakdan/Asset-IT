335ab86552856f759257c54d55252915cfdec008
Dakdan <khowit7@gmail.com>
2026-01-27 08:30:55 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,10 +1,30 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzNSgpYNigJX7W-RUPq8SLN4e687pE55p72KsbM-nWFcPefKDhjzYAflsm78i42IW7qrw/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzNSgpYNigJX7W-RUPq8SLN4e687pE55p72KsbM-nWFcPefKDhjzYAflsm78i42IW7qrw/exec";
 
-async function callAPI(action, data = {}) {
-  const res = await fetch(API_URL, {
-    method: "POST",
-    headers: { "Content-Type": "text/plain;charset=utf-8" },
-    body: JSON.stringify({ action, ...data })
+function callAPI(action, params = {}) {
+  return new Promise((resolve, reject) => {
+    const cbName = "cb_" + Date.now() + Math.floor(Math.random() * 1000);
+
+    window[cbName] = res => {
+      resolve(res);
+      delete window[cbName];
+      script.remove();
+    };
+
+    const query = new URLSearchParams({
+      action,
+      callback: cbName,
+      ...params
+    }).toString();
+
+    const script = document.createElement("script");
+    script.src = GAS_URL + "?" + query;
+    script.onerror = () => {
+      reject(new Error("Load GAS failed"));
+      delete window[cbName];
+      script.remove();
+    };
+
+    document.body.appendChild(script);
   });
-  return res.json();
 }
+
