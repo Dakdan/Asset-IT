ff405a0e97849fca45e1c5baf78770b253bc0ae5
Dakdan <khowit7@gmail.com>
2026-01-27 10:24:25 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,30 +1,9 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzNSgpYNigJX7W-RUPq8SLN4e687pE55p72KsbM-nWFcPefKDhjzYAflsm78i42IW7qrw/exec";
+const API = 'https://script.google.com/macros/s/AKfycbzNSgpYNigJX7W-RUPq8SLN4e687pE55p72KsbM-nWFcPefKDhjzYAflsm78i42IW7qrw/exec';
 
-function callAPI(action, params = {}) {
-  return new Promise((resolve, reject) => {
-    const cbName = "cb_" + Date.now() + Math.floor(Math.random() * 1000);
-
-    window[cbName] = res => {
-      resolve(res);
-      delete window[cbName];
-      script.remove();
-    };
-
-    const query = new URLSearchParams({
-      action,
-      callback: cbName,
-      ...params
-    }).toString();
-
-    const script = document.createElement("script");
-    script.src = GAS_URL + "?" + query;
-    script.onerror = () => {
-      reject(new Error("Load GAS failed"));
-      delete window[cbName];
-      script.remove();
-    };
-
-    document.body.appendChild(script);
-  });
+async function callAPI(action, data = {}) {
+  const params = new URLSearchParams({ action, ...data });
+  const res = await fetch(`${API}?${params.toString()}`);
+  return res.json();
 }
+
 
